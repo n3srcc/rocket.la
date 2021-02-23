ad18892434061a29d440c24fa2499329f48992e4
Eduardo Mondragon <you@example.com>
2021-02-22 22:48:13 -0600

    finished

--- 2_poke_ground.sh
+++ 2_poke_ground.sh
@@ -2,9 +2,9 @@
 # Eduardo Mondragon
 
 POKE_API="https://pokeapi.co/api/v2/type/5/"
-
 JSON_FILE="pokeGround.json"
 CSV_EXPORTED="api/twoslotgroundpokemons.csv"
+
 curl -sS $POKE_API -o $JSON_FILE && cat $JSON_FILE \
     | grep -Po '{"name":"([a-z]+)","url":"(https?:\/\/[\w\/\]+\.]+)"},"slot":(2)}' \
     | sed -rnE 's/\{"name":"(.*)","url":"(.*)"\},"slot":2}/\1,\2,2/p' > $CSV_EXPORTED && echo "CSV exported $CSV_EXPORTED"
